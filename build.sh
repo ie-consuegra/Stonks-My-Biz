d336a0293840a931efebf2c90afbba39a4686693
Ivan Consuegra <ie.consuegra@gmail.com>
2021-04-22 10:26:48 -0500

    Optimize building of files for production
    Now build.sh bundles the code and generates final backend and frontend files ready for test or deploy

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ echo "Done, file can be found as backend.js in dist folder"
 echo "Bundling frontend JavaScript code..."
 cat ./client/js/utils.js ./client/js/SmartTable.js ./client/js/appConfig.js ./client/js/globals.js ./client/js/switchView.js ./client/js/output.js ./client/js/serverCalls.js ./client/js/input.js ./client/js/onload.js ./client/js/datePickerSetup.js > ./client/bundle.js
 echo "Done, file can be found as bundle.js in client folder"
-echo "Run npm run build-frontend to create the frontend.html file"
+echo "Building frontend file..."
+npx html-build -c html-build-config.js client/index.html dist/frontend.html
+echo "Done, ready to test or deploy"
