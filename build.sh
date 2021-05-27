7ac5444b34a8b1c6ca842a606eab9a4ff80b32d3
Ivan Consuegra <ie.consuegra@gmail.com>
2021-05-27 11:38:41 -0500

    Optimize pushing to Google Drive
    Run sh deploy.sh to bundle and upload

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-echo "Bundling backend JavaScript code..."
+echo "*  Bundling backend JavaScript code..."
 cat ./server/utils.js ./server/DB.js ./server/DBS.js ./server/stonksApp.js ./server/app.js ./server/server.js > ./dist/backend.js
-echo "Done, file can be found as backend.js in dist folder"
-echo "Bundling frontend JavaScript code..."
+echo "✔️  Done, file can be found as backend.js in dist folder"
+printf "\n"
+echo "*  Bundling frontend JavaScript code..."
 cat ./client/js/utils.js ./client/js/SmartTable.js ./client/js/settings.js ./client/js/globals.js ./client/js/switchView.js ./client/js/output.js ./client/js/serverCalls.js ./client/js/input.js ./client/js/computations.js ./client/js/onload.js ./client/js/datePickerSetup.js > ./client/bundle.js
-echo "Done, file can be found as bundle.js in client folder"
-echo "Building frontend file..."
+echo "✔️  Done, file can be found as bundle.js in client folder"
+printf "\n"
+echo "*  Building frontend file..."
 npx html-build -c html-build-config.js client/index.html dist/frontend.html
-echo "Done, ready to test or deploy"
+echo "✔️  Done, ready to test or deploy"
+printf "\n"
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+sh ./build.sh
+echo "🔼  Uploading..."
+clasp push
+echo "✔️  Done! Execution finalized at: $(date +"%T")"
